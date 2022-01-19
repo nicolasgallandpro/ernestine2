389843888139d4271483bbb9b8cbda33b464c034
nicofluks <nicolas.galland.pro@gmail.com>
2022-01-19 01:35:39 +0100

    ...

--- input/example.rsc
+++ input/example.rsc
@@ -5,16 +5,22 @@ refresh_time: "* * * *"
 formatting:
   - "title": true
   - "author": true
-feeds:
-    - 'le monde pixels': https://www.lemonde.fr/pixels/rss_full.xml
-    - 'le monde planete': https://www.lemonde.fr/planete/rss_full.xml
-    - 'chaine youtube x': https://www.youtube.com/channel/UCU0FhLr6fr7U9GOn6OiQHpQ
-    - 'chaine youtube y': https://www.youtube.com/channel/UC3E2DhYIqnoc6H3WXwTVnlA
-    - 'stat news': https://www.statnews.com/feed/
-    - 'blog le monde realites biomedicales': https://www.lemonde.fr/blog/realitesbiomedicales/feed/
-    - 'nyt science': https://rss.nytimes.com/services/xml/rss/nyt/Science.xml
-    - 'podcast x': http://radiofrance-podcast.net/podcast09/rss_14312.xml
-    - 'podcast y': http://radiofrance-podcast.net/podcast09/rss_20902.xml   #commentaire 
-    - 'google news paleoanthropologie fr': 'googlenews-fr-fr: paléoanthropologie'
-    - 'google news tribunes' : 'googlenews-fr-fr: tribune'
-      keep_only: "'Tribune de Genève' not in source and 'Tribune de lyon' not in source and 'latribune' not in domain and 'herault-tribune' not in domain"
+categories:
+  - name: 'paleoanthropologie'
+    feeds:
+      - 'le monde pixels': https://www.lemonde.fr/pixels/rss_full.xml
+      - 'le monde planete': https://www.lemonde.fr/planete/rss_full.xml
+      - 'chaine youtube x': https://www.youtube.com/channel/UCU0FhLr6fr7U9GOn6OiQHpQ
+      - 'chaine youtube y': https://www.youtube.com/channel/UC3E2DhYIqnoc6H3WXwTVnlA
+      - 'stat news': https://www.statnews.com/feed/
+      - 'nyt science': https://rss.nytimes.com/services/xml/rss/nyt/Science.xml
+      - 'podcast x': http://radiofrance-podcast.net/podcast09/rss_14312.xml
+      - 'podcast y': http://radiofrance-podcast.net/podcast09/rss_20902.xml   #commentaire 
+      - 'google news paleoanthropologie fr': 'googlenews-fr-fr: paléoanthropologie'
+  - name: 'biologie'
+    feeds:
+      - 'blog le monde realites biomedicales': https://www.lemonde.fr/blog/realitesbiomedicales/feed/
+  - name: 'tribunes'
+    feeds:
+      - 'google news tribunes' : 'googlenews-fr-fr: tribune'
+        keep_only: "'Tribune de Genève' not in source and 'Tribune de lyon' not in source and 'latribune' not in domain and 'herault-tribune' not in domain"
--- sources/rsc.py
+++ sources/rsc.py
@@ -20,32 +20,77 @@ def parse_rsc_file(rsc_file):
     # name
     if 'name' not in rsc:
         raise Exception("The rsc file as no 'name' entry")
-    for i, inp in enumerate(rsc['feeds']):
-        possibles_names = []
-        for entry_key in inp.keys():
-            if entry_key not in KEYWORDS:
-                possibles_names.append(entry_key)
-        if len(possibles_names) == 0:
-            raise Exception(f'The input number {i} has no name')
-        if len(possibles_names)>1:
-           raise Exception(f'The input number {i} has {len(possibles_names)} possible names : {",".join(possibles_names)}')
-        inp['inpuut'] = inp[possibles_names[0]]
-        inp['name'] = possibles_names[0]
+    for category in rsc['categories']:
+        for i, inp in enumerate(category['feeds']):
+            possibles_names = []
+            for entry_key in inp.keys():
+                if entry_key not in KEYWORDS:
+                    possibles_names.append(entry_key)
+            if len(possibles_names) == 0:
+                raise Exception(f'The input number {i} has no name')
+            if len(possibles_names)>1:
+                raise Exception(f'The input number {i} has {len(possibles_names)} possible names : {",".join(possibles_names)}')
+            inp['inpuut'] = inp[possibles_names[0]]
+            inp['name'] = possibles_names[0]
+            del inp[possibles_names[0]]
             
     return rsc
 
 
+def get_raw_posts(rsc_conf):
+    """
+    Retrieve not filtered posts for a rsc conf. 
+    Args : Expected format for rsc_conf : [
+        'name':'...', ..., 
+        categories:[
+            name: "categoryX",
+            'feeds':[{
+                name:'...',
+                inpuut:'htpps://.....'
+            }, ...]
+    ] 
+    Output : dictionnary whose keys are inpuuts (rss urls, youtube channel, ....) and values are rss data :
+        {'inpuutxxxx': __rss_data__ }
+    """
+    inputs = []
+    for category in rsc_conf['categories']:
+        inputs += [ feed['inpuut'] for feed in category['feeds']]
+    raw = retrieve_inputs(inputs)
+    return raw
+
+def prepare_curation_data_without_thumbnails(rsc_conf, raw_results, time_filtered=False):
+    """
+    Makes a well formated object with categories, sources, ordered posts
+    """
+    if time_filtered:
+        raise NotImplementedError()
+    #if pas google news : ajouter la source 
+    print()
 
 
-
+def print_formated_posts(formated_posts):
+    """
+    """
+    for category in formated_posts.categories:
+        print('---------------', category.name)
+        for post in category.posts:
+            print(post.published, post.title)
+            print(post.url)
 
 
 #---------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     from pprint import pprint
-    pprint(parse_rsc_file("/Users/nicolas/Documents/dev/ernestine/ernestine2/input/example.rsc"))
+    
+    rsc_conf = parse_rsc_file("/Users/nicolas/Documents/dev/ernestine/ernestine2/input/example.rsc")
+    pprint(rsc_conf)
 
+    raw = get_raw_posts(rsc_conf)
+    print(raw.keys())
+
+    formated_posts = prepare_curation_data_without_thumbnails(rsc_conf, raw_results, time_filtered=False) 
+    print_formated_posts(formated_posts) 
     
